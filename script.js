ef555f57b88f515ed01325a244ea05b07a137e3b
Neharika Singh <neharikasingh@gmail.com>
2025-05-04 16:12:06 +0530

    changed some UI

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// const input_text = document.querySelector(".input-text")
-// const add_button = document.getElementById("#add-btn")
 const input_text = document.querySelector("#todo-input"); // corrected selector
 const add_button = document.getElementById("add-btn"); // removed extra '#'
 const task_list = document.getElementById("task-list"); // properly selected UL
@@ -7,10 +5,8 @@ const add = () => {
     const text = input_text.value.trim() // get input value
     if (text !== "") {
         const delete_icon = "delete_icon.jpg";
-        const li = document.createElement('li');
+        const li = document.createElement('div');
         li.classList.add("task-item");  // Add the task-item class here
-        // li.innerText = text
-        // li.innerHTML = `${text}`;
         li.innerHTML =  `<span>${text} </span> <img src="${delete_icon}" class="delete" />`;
         task_list.appendChild(li);
         input_text.value = " ";
